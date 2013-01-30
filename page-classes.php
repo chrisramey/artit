16bacb83228b9a8b6a974629927b9363cf297edb
chrisramey <cjramey@gmail.com>
2013-01-30 08:29:49 -0600

    Added initial blog template & class description pages

--- page-classes.php
+++ page-classes.php
@@ -1,19 +1,24 @@
 <?php get_header() ?>
 
 <?php if(have_posts()): ?>
-	<?php while(have_posts()): the_post()?>
+	<?php while(have_posts()): the_post() ?>
 		<div class="page-wrapper">
 			<h2><?php the_title() ?></h2>
 			<?php the_content() ?>
+
 			<?php 
 			// Get pages for class descriptions
+			$classes = get_pages(array(
+				'child_of' => $post->ID
+			));
 
 			// Display class names & descriptions
-
-			// Get page for schedule
-
-			// Display schedule
-			?>
+			foreach($classes as $class) : ?>
+				<div class="course">
+					<h3><?php echo $class->post_title ?></h3>
+					<?php echo apply_filters('the_content',$class->post_content) ?>
+				</div>
+			<?php endforeach ?>
 		</div>
 	<?php endwhile ?>
 <?php endif ?>
